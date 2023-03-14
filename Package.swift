1316920fc8c38fb2627940ea832ab805fd38d667
josh150 <69014339+josh150@users.noreply.github.com>
2023-03-14 14:21:37 -0400

    Update Package.swift
    Update platform versions to match Introspect

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "ScrollViewProxy",
     platforms: [
-        .macOS(.v10_13),
-        .iOS(.v11),
-        .tvOS(.v11)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
